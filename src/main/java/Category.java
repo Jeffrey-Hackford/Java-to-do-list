67c24ee84e2867d3a3296bc217d547d0543a0ad6
Ryan Harvey and Sam Durfey <ryan@>
2016-05-02 16:05:27 -0700

    more updates for sql. frontend will be added shortly.

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,47 +1,65 @@
-import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
 public class Category {
-  private String mName;
-  private static ArrayList<Category> instances = new ArrayList<Category>();
-  private int mId;
-  private ArrayList<Task> mTasks;
+  private int id;
+  private String name;
 
   public Category(String name) {
-    mName = name;
-    instances.add(this);
-    mId = instances.size();
-    mTasks = new ArrayList<Task>();
-  }
-
-  public ArrayList<Task> getTasks() {
-    return mTasks;
-  }
-
-  public void addTask(Task task) {
-    mTasks.add(task);
+    this.name = name;
   }
 
   public String getName() {
-    return mName;
-  }
-
-  public static ArrayList<Category> all() {
-    return instances;
-  }
-
-  public static void clear() {
-    instances.clear();
+    return name;
   }
 
   public int getId() {
-    return mId;
+    return id;
+  }
+
+  public static List<Category> all() {
+    String sql = "SELECT id, name FROM Categories";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Category.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherCategory) {
+    if (!(otherCategory instanceof Category)) {
+      return false;
+    } else {
+      Category newCategory = (Category) otherCategory;
+      return this.getName().equals(newCategory.getName()) &&
+             this.getId() == newCategory.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO Categories(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
   }
 
   public static Category find(int id) {
-    try {
-      return instances.get(id - 1);
-    } catch (IndexOutOfBoundsException e) {
-      return null;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM Categories where id=:id";
+      Category category = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Category.class);
+      return category;
+    }
+  }
+  public List<Task> getTasks() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where categoryId=:id";
+      return con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeAndFetch(Task.class);
     }
   }
 }
